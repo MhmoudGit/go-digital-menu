280c05d6f43f0dceb9821fdc7f305822e2b36f78
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2023-09-07 01:02:49 +0300

    database

---
+++ database/connection.go
@@ -0,0 +1,51 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var Db *gorm.DB
+
+func Connect() {
+	var err error
+	// Load the environment variables from the .env file
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("Error loading .env file")
+	}
+	dbUserName := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	// Create a connection to the PostgreSQL database.
+	dsn := fmt.Sprintf("user=%v password=%v dbname=%v sslmode=disable", dbUserName, dbPassword, dbname)
+
+	// Open db connection
+	Db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %v", err)
+	} else {
+		log.Fatal("database connected successfully...")
+	}
+}
+
+func AutoMigrateDb(models []interface{}) {
+	// // AutoMigrate will create tables if they don't exist based on the model structs.
+	err := Db.AutoMigrate(models)
+	if err != nil {
+		log.Fatalf("Error migrating database tables: %v", err)
+	}
+	log.Fatal("Tables created successfully...")
+}
+
+func Close() {
+	// Close db
+	dbInstance, _ := Db.DB()
+	_ = dbInstance.Close()
+	log.Fatal("database is closed successfully...")
+}
--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"github.com/MhmoudGit/go-digital-menu/database"
+	"github.com/MhmoudGit/go-digital-menu/models"
+)
+
+// Define a slice of model structs that you want to migrate.
+var modelsToMigrate = []interface{}{
+	models.Category{},
+	models.Product{},
+	// Add more model structs here if needed.
+}
 
 func main() {
-	fmt.Println("hello world!")
+	database.Connect()
+	database.AutoMigrateDb(modelsToMigrate)
+	defer database.Close()
 }
