63e9bac4c374d440e510b3200062f305690a2989
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2023-09-07 05:48:07 +0300

    models

--- models/provider.go
+++ models/provider.go
@@ -3,6 +3,7 @@ package models
 import (
 	"time"
 
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
@@ -26,4 +27,20 @@ type Provider struct {
 	IsActive    bool `gorm:"not null, default:'true'"`
 	Categories  []Category
 	Products    []Product
+}
+
+// Verify Password.
+func (p *Provider) VerifyPassword(password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(p.Password), []byte(password))
+	return err
+}
+
+// HashPassword securely hashes the provided password and sets it in the PasswordHash field.
+func (p *Provider) HashPassword(password string) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	p.Password = string(hashedPassword)
+	return nil
 }
