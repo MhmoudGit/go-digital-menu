964305014eb7286dcdb99ff945b084281b6f1b54
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2023-09-07 06:29:15 +0300

    products_model

--- models/product.go
+++ models/product.go
@@ -19,7 +19,26 @@ type Product struct {
 }
 
 type Options struct {
-	Name   string
-	EnName string
-	Price  int
+	Name   string `json:"name"`
+	EnName string `json:"enName"`
+	Price  int    `json:"price"`
+}
+
+// schemas
+type PostProduct struct {
+	Name       string    `json:"name"`
+	EnName     string    `json:"enName"`
+	Details    string    `json:"details"`
+	EnDetails  string    `json:"enDetails"`
+	Image      string    `json:"image"`
+	Price      int       `json:"price"`
+	Options1   []Options `json:"options1"`
+	IsActive   bool      `json:"isActive"`
+	CategoryID uint      `json:"categoryId"`
+	ProviderID uint      `json:"providerId"`
+}
+
+type GetProduct struct{
+	gorm.Model
+	PostProduct
 }
