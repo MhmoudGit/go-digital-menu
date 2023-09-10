abd71ed70e1d23501269db10e0e6a1266207faa2
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2023-09-10 15:10:36 +0300

    providers

--- models/provider.go
+++ models/provider.go
@@ -9,24 +9,24 @@ import (
 
 type Provider struct {
 	gorm.Model
-	Email       string `gorm:"not null;index;unique"`
-	Password    string `gorm:"not null"`
-	Image       string
-	Name        string `gorm:"not null"`
-	EnName      string `gorm:"not null"`
-	ServiceType string `gorm:"not null"`
-	Whatsapp    string `gorm:"not null"`
-	Phone       string `gorm:"not null"`
-	Address     string `gorm:"not null"`
-	EnAddress   string `gorm:"not null"`
-	Facebook    string
-	Theme       string    `gorm:"not null"`
-	OpenedFrom  time.Time `gorm:"not null"`
-	OpenedTo    time.Time `gorm:"not null"`
-	Url         string
-	IsActive    bool `gorm:"not null, default:'true'"`
-	Categories  []Category
-	Products    []Product
+	Email       string     `gorm:"not null;index;unique" json:"email"`
+	Password    string     `gorm:"not null" json:"-"`
+	Image       string     `json:"image"`
+	Name        string     `gorm:"not null" json:"name"`
+	EnName      string     `gorm:"not null" json:"enName"`
+	ServiceType string     `gorm:"not null" json:"serviceType"`
+	Whatsapp    string     `gorm:"not null" json:"whatsapp"`
+	Phone       string     `gorm:"not null" json:"phone"`
+	Address     string     `gorm:"not null" json:"adress"`
+	EnAddress   string     `gorm:"not null" json:"enAdress"`
+	Facebook    string     `json:"facebook"`
+	Theme       string     `gorm:"not null" json:"theme"`
+	OpenedFrom  time.Time  `gorm:"not null" json:"openedFrom"`
+	OpenedTo    time.Time  `gorm:"not null" json:"openedTo"`
+	Url         string     `json:"url"`
+	IsActive    bool       `gorm:"not null, default:'true'" json:"isActive"`
+	Categories  []Category `json:"-"`
+	Products    []Product  `json:"-"`
 }
 
 // Verify Password.
@@ -43,4 +43,42 @@ func (p *Provider) HashPassword(password string) error {
 	}
 	p.Password = string(hashedPassword)
 	return nil
+}
+
+type PostProvider struct {
+	Email       string    `json:"email"`
+	Password    string    `json:"password"`
+	Image       string    `json:"image"`
+	Name        string    `json:"name"`
+	EnName      string    `json:"enName"`
+	ServiceType string    `json:"serviceType"`
+	Whatsapp    string    `json:"whatsapp"`
+	Phone       string    `json:"phone"`
+	Address     string    `json:"adress"`
+	EnAddress   string    `json:"enAdress"`
+	Facebook    string    `json:"facebook"`
+	Theme       string    `json:"theme"`
+	OpenedFrom  time.Time `json:"openedFrom"`
+	OpenedTo    time.Time `json:"openedTo"`
+	Url         string    `json:"url"`
+}
+
+type UpdateProvider struct {
+	Email       string    `json:"email"`
+	Name        string    `json:"name"`
+	EnName      string    `json:"enName"`
+	ServiceType string    `json:"serviceType"`
+	Whatsapp    string    `json:"whatsapp"`
+	Phone       string    `json:"phone"`
+	Address     string    `json:"adress"`
+	EnAddress   string    `json:"enAdress"`
+	Facebook    string    `json:"facebook"`
+	Theme       string    `json:"theme"`
+	OpenedFrom  time.Time `json:"openedFrom"`
+	OpenedTo    time.Time `json:"openedTo"`
+	Url         string    `json:"url"`
+}
+
+type UpdateProviderImage struct {
+	Image string `json:"image"`
 }
