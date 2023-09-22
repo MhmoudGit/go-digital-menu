3245c5079a1d53b053f2570b3dd49e3760fd1579
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2023-09-22 10:02:24 +0300

    image-type

--- utils/upload_file.go
+++ utils/upload_file.go
@@ -6,17 +6,26 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 )
 
 func UploadFile(w http.ResponseWriter, r *http.Request, fieldName string) string {
 	// Get the file from the form
-	file, _, err := r.FormFile(fieldName) // "file" is the name of the file input field in your form
+	file, header, err := r.FormFile(fieldName) // "file" is the name of the file input field in your form
 	if err != nil {
 		http.Error(w, "Unable to get file from form", http.StatusBadRequest)
 		return ""
 	}
 	defer file.Close()
+
+	// Validate file type (allow only PNG and JPG)
+	allowedExtensions := []string{".png", ".jpg", ".jpeg"}
+	ext := filepath.Ext(header.Filename)
+	if !isValidExtension(ext, allowedExtensions) {
+		http.Error(w, "Invalid file type", http.StatusBadRequest)
+		return ""
+	}
 
 	// Generate a unique filename for the uploaded file
 	uniqueFilename := generateUniqueFilename()
@@ -39,6 +48,16 @@ func UploadFile(w http.ResponseWriter, r *http.Request, fieldName string) string
 
 	// return the file name
 	return fmt.Sprintf("/uploads/%v", uniqueFilename)
+}
+
+func isValidExtension(extension string, allowedExtensions []string) bool {
+	extension = strings.ToLower(extension)
+	for _, ext := range allowedExtensions {
+		if ext == extension {
+			return true
+		}
+	}
+	return false
 }
 
 func generateUniqueFilename() string {
